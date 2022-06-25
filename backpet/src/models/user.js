d981f8e9a69a25de6da5c8ce3bc99a5ff1a5a4fb
Rafael <rafael.trindade@edu.unifil.br>
2022-06-24 22:42:03 -0300

    implementada parte do crud de usuário

--- backpet/src/models/user.js
+++ backpet/src/models/user.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcryptjs');
 const knex = require('../services/db');
 const { PASSWORD_SALT } = require('../../config/general.config');
 
-// get users schema from knex
 const schema = async () => {
   return await knex.table('users').columnInfo();
 };
@@ -11,17 +10,52 @@ const hashPassword = async password => {
   return await bcrypt.hash(password, PASSWORD_SALT);
 };
 
-const create = async data => {};
+const create = async data => {
+  try {
+    const user = await knex('users').insert(data).returning(['id', 'username']);
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
 
-const getAll = async () => {};
+const getAll = async () => {
+  const users = await knex('users').select(
+    'id',
+    'username',
+    'phone',
+    'email',
+    'city',
+    'uf',
+    'adopter_score',
+    'donor_score'
+  );
 
-const getById = async id => {};
+  return users;
+};
 
-const getByCredentials = async (username, password) => {};
+const getById = async id => {
+  const [user] = await knex('users').where('id', id);
+  if (!user) return;
 
+  delete user.password;
+  return user;
+};
+
+const getByCredentials = async (username, password) => {
+  const user = await knex('users').where('username', username)[0];
+  if (user.length === 0) return;
+  const correctPassword = await bcrypt.compare(password, user.password);
+  if (correctPassword) return user;
+};
+
+// TODO
 const update = async (id, data) => {};
 
-const remove = async id => {};
+const remove = async id => {
+  const user = await knex('users').where('id', id).del();
+  return { id };
+};
 
 module.exports = {
   schema,
