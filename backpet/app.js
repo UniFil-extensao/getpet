9b6ea98a9c2e91be556028219dd2fd7045fe458a
Gustavo Queiroz <compgustavo@edu.unifil.br>
2022-06-26 13:38:54 -0300

    requisições funcionando

--- backpet/app.js
+++ backpet/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const errorHandler = require('./src/middleware/error');
 const userRoutes = require('./src/routes/users');
+const cors = require('cors');
+const helmet = require('helmet');
+
+app.use(helmet()); 
+app.use(cors({
+  credentials: true,
+  origin: true
+}));
 
 app.use(express.json());
 app.use('/users', userRoutes);
