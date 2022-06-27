e7dc1a01dff7951a70b5f4df95527c2401f5c188
Rafael <rafael.trindade@edu.unifil.br>
2022-06-27 19:32:27 -0300

    ajustes de configuração

--- backpet/app.js
+++ backpet/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const app = express();
-const errorHandler = require('./src/middleware/error');
-const userRoutes = require('./src/routes/users');
 const cors = require('cors');
 const helmet = require('helmet');
 
-app.use(helmet()); 
-app.use(cors({
-  credentials: true,
-  origin: true
-}));
+const errorHandler = require('./src/middleware/error');
+const userRoutes = require('./src/routes/users');
+
+const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+
 app.use('/users', userRoutes);
 
 // REM: remover no futuro
