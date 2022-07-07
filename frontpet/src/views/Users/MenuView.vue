759c3baf92174158701b5591621152eeeaedd1fb
WesleyKasteller <WesleyKCarlesso@edu.unifil.br>
2022-07-07 12:53:19 -0300

    adicionado menu no index

---
+++ frontpet/src/views/Users/MenuView.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="divMenu">
+    <button id="btnMenu" style="margin-left: 35%; padding: 10px; border: none">
+        <img src="https://www.escolagames.com.br/img/thumbs/jogos/zoo-louco.jpg" style="height: 40px">
+        <br>
+        Todos
+    </button>
+    <button id="btnMenu" style="margin-left: 5%; padding: 10px; border: none">
+        <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/streams/2013/May/130522/6C7536084-g-hlt-120105-puppy-423p.jpg" style="height: 40px">
+        <br>
+        Cachorros
+    </button>
+    <button id="btnMenu" style="margin-left: 5%; padding: 10px; border: none">
+        <img src="https://i.insider.com/61d1c0e2aa741500193b2d18?width=1136&format=jpeg" style="height: 40px">
+        <br>
+        Gatos
+    </button>
+    <!-- <button id="btnMenu" style="margin-left: 5%; padding: 10px; border: none">
+        <img src="https://www.teclasap.com.br/wp-content/uploads/2014/07/horse-1.jpg" style="height: 40px">
+        <br>
+        Cavalos
+    </button> -->
+</div>
+</template>
